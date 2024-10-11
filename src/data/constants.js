a7159ee9c3856cf7890484eb7813f14d7e291e12
Ahmed Raouf <info@codepressp.net>
2024-10-11 20:04:40 +0300

    firist commit

---
+++ src/data/constants.js
@@ -0,0 +1,652 @@
+import html from '../images/html.svg'
+import css from '../images/css.svg'
+import javascript from '../images/js.svg';
+import typescript from '../images/ts.svg';
+import angular from '../images/angular.svg';
+import npm from '../images/npm.svg';
+import tailwind from '../images/tailwind.svg';
+import bootstrap from '../images/bootstrap.svg';
+import sass from '../images/sass.svg';
+import node from '../images/node.svg';
+import express from '../images/express.svg';
+import swagger from '../images/swagger.svg';
+import nestjs from '../images/nestjs.svg';
+import graphql from '../images/graphql.svg';
+import REST from '../images/api.svg';
+import postgresql from '../images/postgresql.svg';
+import mongo from '../images/mongo.svg';
+import cloudinary from '../images/cloudinary.svg';
+import Csharp from '../images/c-sharp.svg';
+import dotnet from '../images/dotnet.svg';
+import microsoftsqlserver from '../images/microsoft-sql-server.svg';
+import aws from '../images/aws.svg';
+import googlecloud from '../images/google-cloud.svg';
+import docker from '../images/docker.svg';
+import nginx from '../images/nginx.svg';
+import git from '../images/git.svg';
+import github from '../images/github.svg';
+import vercel from '../images/vercel.svg';
+import netlify from '../images/netlify.svg';
+import vscode3 from '../images/vscode3.svg';
+import webstorm from '../images/webstorm.svg';
+import visualstudio from '../images/visualstudio.svg';
+import postman from '../images/postman.svg';
+import jira from '../images/jira.svg';
+import slack from '../images/slack.svg';
+import figma from '../images/figma.svg';
+import sockit from '../images/sockit.webp';
+import bluehost from '../images/bluehost.svg';
+import mti from '../images/mti.png';
+import route from '../images/route.jpg';
+import oreilly from '../images/OReilly.jpg';
+import udemy from '../images/Udemy.png';
+import odc from '../images/odc.jpg';
+import e01lectronics from '../images/01electronics.ico';
+import booksortz from '../images/booksortz.jpeg';
+import ischool from '../images/ischool.jpg';
+import codepresso from '../images/codepresso.png';
+import nilesons from '../images/nilesons.png';
+import oasis from '../images/oasis.png'
+import movies_app from '../images/Movies_Api.png'
+import Fokir from '../images/fokir.png'
+import SmartLogin from '../images/SmartLogin.png'
+import devfolio from '../images/dev-folio.jpg'
+import egyption_party from '../images/egyption_party.png'
+import Simon from '../images/Simon.png'
+import bookMark from '../images/bookMark.png'
+import Weather from '../images/Weather.png'
+import Angora from '../images/Angora.png'
+import library_management_system from '../images/library management system image.jpg'
+import QR_Attend from '../images/QR-Attend.png'
+import product_management_app from '../images/product-management-app.png'
+export const Bio = {
+  name: "Ahmed Raouf",
+  roles: [
+    "Software Developer",
+    "Web Developer",
+    "Frontend Developer",
+    "Backend Developer",
+    "Full Stack Developer",
+  ],
+  description: "Hello I'm Ahmed Raouf a Full Stack Web Developer | Passionate about Building Scalable and High-Performance Applications.",
+  github: "https://github.com/Ahmed-Raouf-Metwally",
+  resume: "http://aiapply.co/r/6708c024944bf",
+  linkedin: "https://www.linkedin.com/in/ahmed-abdul-raouf/",
+  facebook: "https://www.facebook.com/AhmedRaouf.Metwally/",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "HTML",
+        image: html
+      },
+      {
+        name: "CSS",
+        image: css
+      },
+      {
+        name: "JavaScript",
+        image: javascript
+      },
+      {
+        name: "TypeScript",
+        image: typescript
+      },
+      {
+        name: "Angular",
+        image: angular
+      },
+      {
+        name: "npm",
+        image: npm
+      },
+      {
+        name: "TailwindCss",
+        image: tailwind
+      },
+      {
+        name: "Bootstrap",
+        image: bootstrap
+      },
+      {
+        name: "Sass",
+        image: sass
+      }
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node Js",
+        image: node
+      },
+      {
+        name: "Express Js",
+        image: express
+      },
+   
+      {
+        name: "Nest Js",
+        image: nestjs
+      },
+      {
+        name: "Graph Ql",
+        image: graphql
+      },
+      {
+        name: "RESTful API",
+        image: REST
+      },
+      {
+        name: "Swagger",
+        image: swagger
+      },
+      {
+        name: "PostgreSQL",
+        image: postgresql
+      },
+      {
+        name: "MongoDB",
+        image: mongo
+      },
+      {
+        name: "Cloudinary",
+        image: cloudinary
+      },
+      {
+        name: "Sockit.io",
+        image: sockit
+      },
+      {
+        name: "C#",
+        image: Csharp
+      },
+      {
+        name: ".NET",
+        image: dotnet
+      },
+      {
+        name: "Microsoft Sql Server",
+        image: microsoftsqlserver
+      },
+    ],
+  },
+  {
+    title: "DevOps",
+    skills: [
+      {
+        name: "AWS",
+        image: aws
+      },
+      {
+        name: "Google Cloud",
+        image: googlecloud
+      },
+      {
+        name: "Docker",
+        image: docker
+      },
+      {
+        name: "Nginx",
+        image: nginx
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image: git
+      },
+      {
+        name: "GitHub",
+        image: github
+      },
+      {
+        name: "Figma",
+        image: figma
+      },
+      {
+        name: "Vercel",
+        image: vercel
+      },
+      {
+        name: "Netlify",
+        image: netlify
+      },
+      {
+        name: "WebStorm",
+        image: webstorm
+      },
+      {
+        name: "VS Code",
+        image: vscode3
+      },
+      {
+        name: "Visual Studio",
+        image: visualstudio
+      },
+      {
+        name: "Postman",
+        image: postman
+      },
+      {
+        name: "Jira",
+        image: jira
+      },
+      {
+        name: "Slack",
+        image: slack
+      },
+      {
+        name: "Bluehost",
+        image: bluehost
+      },
+    ],
+  },
+];
+
+export const experiences = [
+
+  {
+    id: 0,
+    img: e01lectronics,
+    role: "intern Software Engineer",
+    company: "01 Electronics",
+    date: "Dec 2021 - Feb 2022",
+    desc: "Internship 3 months at 01electronics Electrical Company.",
+    skills: [
+      ".NET Developer",
+      "C#",
+      "Windows Presentation Foundation",
+      "SQL Server",
+      "XAML",
+      "File Zilla FTP",
+      "Technical Expertise",
+    ],
+  },
+  {
+    id: 1,
+    img: e01lectronics,
+    role: "Junior Software Engineer",
+    company: "01 Electronics",
+    date: "Mar 2022 - Jun 2023",
+    desc: "As a Software Engineer at 01electronics Electrical Company, I developed Windows Presentation Foundation (WPF) applications using C# and SQL Server. I contributed to the full software development lifecycle, designing and implementing user-friendly interfaces for enterprise resource planning (ERP), customer relationship management (CRM), and other business modules. My work involved creating new features, optimizing performance, and ensuring seamless database integration with SQL Server to support complex data operations. I collaborated with cross-functional teams to deliver high-quality solutions tailored to client needs while maintaining software scalability and security.",
+    skills: [
+      ".NET Developer",
+      "C#",
+      "Windows Presentation Foundation",
+      "SQL Server",
+      "XAML",
+      "File Zilla FTP",
+      "erp & crm",
+      "Technical Expertise",
+      "Problem-Solving Mindset",
+      "Client-Centric Approach",
+      "On-Time Delivery",
+    ],
+  },
+  {
+    id: 2,
+    img: booksortz,
+    role: "Intern backend developer",
+    company: "Booksportz",
+    date: "Jan 2024 - Mar 2024",
+    desc: "Internship 3 months as Node.JS backend developer intern at DHT (Development hub technology company) (booksportz App)",
+    skills: [
+      "Node.js",
+      "Nestjs",
+      "PostgreSQL",
+      "TypeORM",
+      "GraphQL",
+      "Technical Expertise",
+      "Problem-Solving Mindset",
+      "Client-Centric Approach",
+      "On-Time Delivery",
+    ],
+  },
+  {
+    id: 3,
+    img: booksortz,
+    role: "Scrum Master",
+    company: "Booksportz",
+    date: "Feb 2024 - Mar 2024",
+    desc: "Led agile teams and facilitated scrum ceremonies",
+    skills: [
+      "agile",
+      "scrumming",
+      "Technical Expertise",
+      "Problem-Solving Mindset",
+      "Client-Centric Approach",
+    ],
+  },
+  {
+    id: 4,
+    img: ischool,
+    role: "Coding instructor Intern",
+    company: "ISchool",
+    date: "Jul 2024",
+    desc: "Assisted in teaching coding classes, supported curriculum development, and provided student and technical support. Gained valuable experience in both instruction and coding.",
+    skills: [
+      "Technical Expertise",
+    ]
+
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: mti,
+    school: "Modern Academy in Maadi",
+    date: "sep 2018 - Sep 2022",
+    grade: "Bachelor's Degree",
+    desc: "I graduated from Modern Academy's Computer Science Department, where I gained a solid foundation in software development, data structures, algorithms, and problem-solving. My academic journey included hands-on experience in various programming languages and technologies, preparing me for real-world applications in backend development and system architecture. The coursework and projects allowed me to strengthen my skills in object-oriented programming, databases, and web technologies, aligning my academic knowledge with industry standards.",
+    degree: "Computer Science",
+  },
+  {
+    id: 1,
+    img: route,
+    school: "Route Training Center",
+    date: "Jan 2021 - Oct 2021",
+    grade: "A",
+    desc: "Training and learning by produced code in programming languages such as HTML, CSS, Java-Script, Type-Script , Node.JS and Mongo Database to create the most effective and clean websites with framework Angular & Node.JS.",
+    degree: "MEAN Stack development",
+  },
+  {
+    id: 2,
+    img: oreilly,
+    school: "O'Reilly",
+    date: "Jan 2024 - Mar 2024",
+    grade: "A",
+    desc: "Scrum Fundamentals and agile principles.",
+    degree: "Agile Scrum Master Course",
+  },
+  {
+    id: 3,
+    img: udemy,
+    school: "Udemy",
+    date: "May 2024 - Jul 2024",
+    grade: "A",
+    desc: "Learn Node.js by building real-world applications with Node JS, Express, MongoDB.",
+    degree: "Node.JS Tutorial and Projects Course",
+  },
+  {
+    id: 4,
+    img: odc,
+    school: "orange Digital Center",
+    date: "Sep 2024",
+    grade: "A",
+    desc: "Training and learning by produced code in programming languages such as HTML, CSS, Java-Script, Type-Script , Node.JS and Mongo Database to create the most effective and clean websites with framework Angular & Node.JS.",
+    degree: "MEAN Stack Development Scholarship by Amit Learning",
+  },
+];
+
+export const projects = [
+  {
+    id: 0,
+    title: "Codepresso",
+    description: "A responsive landing site create by native html and css bootstrab and JS.",
+    image: codepresso,
+    tags: [
+      "HTML",
+      "CSS",
+      "JS",
+      "Tailwindcss",
+      "Boostrap",
+      "Responsive",
+      "JQuery"
+    ],
+    category: "landing",
+    webapp: "https://ahmed-raouf-metwally.github.io/codepresso/",
+  },
+  {
+    id: 1,
+    title: "Nile Sons International School",
+    description: "LMS and landing Website for NSIS",
+    image: nilesons,
+    tags: [
+      "HTML",
+      "CSS",
+      "JS",
+      "Tailwindcss",
+      "Boostrap",
+      "Responsive",
+      "JQuery",
+      "Formspree",
+    ],
+    category: "lms",
+    webapp: "https://nilesons.net/",
+  },
+  {
+    id: 2,
+    title: "Oasis",
+    description: "A responsive landing site create by native html and css bootstrab and JS.",
+    image: oasis,
+    tags: [
+      "HTML",
+      "CSS",
+      "JS",
+      "Tailwindcss",
+      "Boostrap",
+      "Responsive",
+      "JQuery"
+    ],
+    category: "landing",
+    webapp: "https://ahmed-raouf-metwally.github.io/Oasis/",
+  },
+  {
+    id: 3,
+    title: "Movie Night",
+    description: "(Movies App) This project displays a new, trending movies, tv shows and actors also, you can search for any of them by using Api , tailwind CSS",
+    image: movies_app,
+    tags: [
+      "HTML",
+      "CSS",
+      "JS",
+      "Tailwindcss",
+      "Boostrap",
+      "Responsive",
+      "JQuery",
+      "AJAX"
+    ],
+    category: "saas",
+    webapp: "https://ahmed-raouf-metwally.github.io/Movies-Apis/",
+  },
+  {
+    id: 4,
+    title: "Fokier",
+    description: "A responsive portfolio page create by native html and css bootstrab and JS.",
+    image: Fokir,
+    tags: [
+      "HTML",
+      "CSS",
+      "JS",
+      "Tailwindcss",
+      "Boostrap",
+      "Responsive",
+      "JQuery",
+    ],
+    category: "landing",
+    webapp: "https://ahmed-maalawi.github.io/Fokir/",
+  },
+  {
+    id: 5,
+    title: "Smart Login",
+    description: "A responsive Login and signup page create by native html and css and jQuery with js.",
+    image: SmartLogin,
+    tags: [
+      "HTML",
+      "CSS",
+      "JS",
+      "Tailwindcss",
+      "Boostrap",
+      "Responsive",
+      "JQuery",
+    ],
+    category: "other",
+    webapp: "https://ahmed-raouf-metwally.github.io/Smart-Login-Signup-Form/",
+  },
+  {
+    id: 6,
+    title: "DEV-FOLIO",
+    description: "Landing Page",
+    image: devfolio,
+    tags: [
+      "HTML",
+      "CSS",
+      "JS",
+      "Tailwindcss",
+      "Boostrap",
+      "Responsive",
+      "JQuery",
+    ],
+    category: "landing",
+    webapp: "https://ahmed-raouf-metwally.github.io/DevFolio/",
+  },
+  {
+    id: 7,
+    title: "Egyptian-party",
+    description: "A responsive landing page for event created with bootstrap and jQuery.",
+    image: egyption_party,
+    tags: [
+      "HTML",
+      "CSS",
+      "JS",
+      "Tailwindcss",
+      "Boostrap",
+      "Responsive",
+      "JQuery",
+    ],
+    category: "landing",
+    webapp: "https://ahmed-raouf-metwally.github.io/Egyption_party/",
+  },
+  {
+    id: 8,
+    title: "Simon",
+    description: "A responsive portfolio page created by bootstrap, fontawesomew and JS.",
+    image: Simon,
+    tags: [
+      "HTML",
+      "CSS",
+      "JS",
+      "Tailwindcss",
+      "Boostrap",
+      "Responsive",
+      "JQuery",
+      "fontawesome",
+    ],
+    category: "landing",
+    webapp: "https://ahmed-raouf-metwally.github.io/Simone/",
+  },
+  {
+    id: 9,
+    title: "E-Book-Mark",
+    description: "A simple application to save websites URLs created by bootstrap and JS",
+    image: bookMark,
+    tags: [
+      "HTML",
+      "CSS",
+      "JS",
+      "Tailwindcss",
+      "Boostrap",
+      "Responsive",
+      "JQuery",
+      "fontawesome",
+    ],
+    category: "saas",
+  },
+  {
+    id: 10,
+    title: "Weather",
+    description: "This project displays a Weather you can search for any country weather of them by using Api , CSS and java-script",
+    image: Weather,
+    tags: [
+      "HTML",
+      "CSS",
+      "JS",
+      "Tailwindcss",
+      "Boostrap",
+      "Responsive",
+      "JQuery",
+      "fontawesome",
+    ],
+    category: "saas",
+    webapp: "https://ahmed-raouf-metwally.github.io/weather/",
+  },
+  {
+    id: 11,
+    title: "Angora",
+    description: "A responsive portfolio page created by bootstrap, fontawesomew and JS (jquery).",
+    image: Angora,
+    tags: [
+      "HTML",
+      "CSS",
+      "JS",
+      "Tailwindcss",
+      "Boostrap",
+      "Responsive",
+      "JQuery",
+      "fontawesome",
+    ],
+    category: "landing",
+    webapp: "https://ahmed-raouf-metwally.github.io/portfolio-Angora-/",
+  },
+  {
+    id: 12,
+    title: "library management system",
+    description: "Library Management System API,  offers a comprehensive solution for libraries to efficiently manage their resources. librarians can effortlessly oversee books, patrons, and borrowing records, streamlining administrative tasks and enhancing the overall library experience.",
+    image: library_management_system,
+    tags: [
+      "Spring Boot",
+      "Maven",
+      "H2 database",
+      "API",
+  
+    ],
+    category: "library",
+    webapp: "https://github.com/Ahmed-Raouf-Metwally/Library-Management-System/",
+  },
+  {
+    id: 13,
+    title: "QR-Attend",
+    description: "This project displays a E-Learning system and capability to record attendance using Qr-code and has the functionality to delete, update his Session, or change its status by using Node.Js(V 16.14.0) and Mongo DB wit framework: Bootstrap CSS, jquery, Express.js, jsonwebtoken, dotenv, qrcode.DB: Atlas Mongo DB , mongoose.Server: heroku.(This App has version Android)",
+    image: QR_Attend,
+    tags: [
+      "Spring Boot",
+      "Maven",
+      "H2 database",
+      "API",
+    ],
+    category: "backend",
+    webapp: "https://github.com/Ahmed-Raouf-Metwally/QR-Attendence-System/",
+  },
+  {
+    id: 14,
+    title: "product-management-app",
+    description: "I developed a full-stack Product Management App with an Angular frontend and an Express.js backend, integrated with MongoDB for database management. The app includes core functionalities such as adding, deleting, updating, and retrieving products, providing a seamless user experience for product management. On the frontend, Angular handles dynamic, responsive UI components, while the backend, built with Express.js, efficiently manages API requests. For authentication, I implemented JWT-based login and sign-up features, using JSON Web Tokens (JWT) and cookies for secure user sessions. The app includes robust validation mechanisms, ensuring data integrity and secure access to the system's features.",
+    image: product_management_app,
+    tags: [
+      "HTML",
+      "CSS",
+      "JS",
+      "Boostrap",
+      "Responsive",
+      "API",
+      "Node JS",
+      "Express JS",
+      "Mongo DB",
+      "Mongoose",
+      "JWT",
+      "Cookies",
+      "Vercel",
+    ],
+    category: "backend",
+    webapp: "https://github.com/Ahmed-Raouf-Metwally/product-management-app-serverSide.git",
+  }
+];
